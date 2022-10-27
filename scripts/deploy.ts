6b43cd502b3b43be319f1fc1656e9dc5c7f014ec
Joe Marron <marronjo@tcd.ie>
2022-10-27 16:08:22 +0100

    ignore config

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,20 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  deployLockContract();
+  deployJoeTokenContract();
+}
+
+async function deployJoeTokenContract(){
+  const JoeToken = await ethers.getContractFactory("Lock");
+  const lock = await JoeToken.deploy();
+
+  await lock.deployed();
+
+  console.log(`Deployed JoeToken Contract Successfully`);
+}
+
+async function deployLockContract(){
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
   const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
@@ -15,8 +29,6 @@ async function main() {
   console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
